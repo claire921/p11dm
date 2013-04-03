30d3893a373d7202375c9aaa6d9bdbea11f19cda
Ying Li <claire921@gmail.com>
2013-04-03 11:43:37 -0400

    Set up test

---
+++ src/KNN.java
@@ -0,0 +1,29 @@
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+
+public class KNN {
+
+	public static void main(String[] args) {
+		DataSource source = null;
+		Instances data = null;
+		try {
+			source = new DataSource("trainProdSelection.arff");
+			data = source.getDataSet();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (data.classIndex() == -1) data.setClassIndex(data.numAttributes() - 1);		
+		 
+		System.out.println("\nDataset:\n");
+		System.out.println(data);
+		 
+		Instance first = data.firstInstance();
+		Instance second = data.get(data.numInstances() - 1);
+		System.out.println(first.classValue());
+		System.out.println(second.classValue());
+	}
+}
